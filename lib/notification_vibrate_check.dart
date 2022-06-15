ae5552d7ba064b4cd69c0b60f1c7fe98064b466b
Tecode <283731869@qq.com>
2022-06-15 23:37:26 +0800

    代码格式化，处理插件

--- lib/notification_vibrate_check.dart
+++ lib/notification_vibrate_check.dart
@@ -1,11 +1,9 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class NotificationVibrateCheck {
-  static const MethodChannel _channel =
-      const MethodChannel('notification_vibrate_check');
+  static const MethodChannel _channel = const MethodChannel('notification_vibrate_check');
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
